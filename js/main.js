2af3900d49458dfede2d340902bd31fde566f769
cch5ng <cch5ng@gmail.com>
2015-08-07 23:51:02 -0700

    added dom parser, used markdown parser. now getting specific quote content to appear dynamically. but some formatting cleanup needed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,95 @@
 
+//helper function
+
+(function(DOMParser) {
+	"use strict";
+
+	var
+	  proto = DOMParser.prototype
+	, nativeParse = proto.parseFromString
+	;
+
+	// Firefox/Opera/IE throw errors on unsupported types
+	try {
+		// WebKit returns null on unsupported types
+		if ((new DOMParser()).parseFromString("", "text/html")) {
+			// text/html parsing is natively supported
+			return;
+		}
+	} catch (ex) {}
+
+	proto.parseFromString = function(markup, type) {
+		if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+			var
+			  doc = document.implementation.createHTMLDocument("")
+			;
+	      		if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+        			doc.documentElement.innerHTML = markup;
+      			}
+      			else {
+        			doc.body.innerHTML = markup;
+      			}
+			return doc;
+		} else {
+			return nativeParse.apply(this, arguments);
+		}
+	};
+}(DOMParser));
+
+//given length of array, returns a random index of the array
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIdx(length) {
+	return Math.floor(Math.random() * (length));
+}
+
 //event handler
 function btnClickHandler(evt) {
-	var hitchcockUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Alfred%20Hitchcock&prop=revisions&rvprop=content&callback=wikiCallback';
+	// var hitchcockUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Alfred%20Hitchcock&prop=revisions&rvprop=content&callback=wikiCallback';
+	// var kubrickUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Stanley%20Kubrick&prop=revisions&rvprop=content&callback=wikiCallback';
+	//var aesopUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Aesop&prop=revisions&rvprop=content&callback=wikiCallback';
+	// var okeefeUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Georgia%20O%27Keeffe&prop=revisions&rvprop=content&callback=wikiCallback';
+	//var grimmUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Brothers%20Grimm&prop=revisions&rvprop=content&callback=wikiCallback';
+	var andersenUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Hans%20Christian%20Andersen&prop=revisions&rvprop=content&callback=wikiCallback';
+
+	// var urlAr = [
+	// 			'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Alfred%20Hitchcock&prop=revisions&rvprop=content&callback=wikiCallback',
+	// 			'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Stanley%20Kubrick&prop=revisions&rvprop=content&callback=wikiCallback',
+	// 			'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Aesop&prop=revisions&rvprop=content&callback=wikiCallback',
+	// 			'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Georgia%20O%27Keeffe&prop=revisions&rvprop=content&callback=wikiCallback',
+	// 			'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Brothers%20Grimm&prop=revisions&rvprop=content&callback=wikiCallback'
+	// ];
 
 	$.ajax({
 		dataType: 'jsonp',
-		url: hitchcockUrl,
+		url: andersenUrl, //urlAr[getRandomIdx(urlAr.length)],
 		success: function(data) {
 			//console.log('got data');
 			//console.log(data);
-			var content = data.query.pages['3483']['revisions']['0']['*'];
-			console.log(content);
+			var content = data.query.pages['757']['revisions']['0']['*']; //andersen
+			//var content = data.query.pages['37737']['revisions']['0']['*']; //grimm
+			//var content = data.query.pages['79']['revisions']['0']['*']; //aesop
+			//var content = data.query.pages['3483']['revisions']['0']['*']; //hitchcock
+			//console.log(content);
+
+			var htmlData = markdown.toHTML(content);
+			console.log(htmlData);
+			var htmlDataNoBreaks = htmlData.replace(/\r|\n/g, '');
+
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(htmlDataNoBreaks, 'text/html');
+			console.log(doc);
+
+			var quotes = doc.querySelectorAll('li > p'); //getElementsByTagName('li').$find('p');
+			console.log(quotes);
+
+			var quotesText = [];
+			for (var i = 0; i < quotes.length; i++) {
+				quotesText.push(quotes[i].innerHTML);
+			}
+
+			console.log(quotesText);
+
+			$('.quote').text(quotesText[getRandomIdx(quotesText.length)]);
 		}
 	});
 
