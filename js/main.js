7315bcbb0bdc6edf285c7a15a3d8adc7627ba72b
cch5ng <cch5ng@gmail.com>
2015-08-07 20:57:04 -0700

    cleaned comments. added btn click listener and handler

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-$(document).ready(function() {
-  $('.text-primary').text('hi codepen!');
-});
 
-var hitchcockUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Alfred%20Hitchcock&prop=revisions&rvprop=content&callback=wikiCallback';
+//event handler
+function btnClickHandler(evt) {
+	var hitchcockUrl = 'https://en.wikiquote.org/w/api.php?format=json&action=query&titles=Alfred%20Hitchcock&prop=revisions&rvprop=content&callback=wikiCallback';
 
-$.ajax({
-	dataType: 'jsonp',
-	url: hitchcockUrl,
-	//data: data,
-	success: function(data) {
-		//console.log('got data');
-		//console.log(data);
-		var content = data.query.pages['3483']['revisions']['0']['*'];
-		console.log(content);
-	}
-});
+	$.ajax({
+		dataType: 'jsonp',
+		url: hitchcockUrl,
+		success: function(data) {
+			//console.log('got data');
+			//console.log(data);
+			var content = data.query.pages['3483']['revisions']['0']['*'];
+			console.log(content);
+		}
+	});
+
+}
+
+//event listener
+var btn = document.getElementById('quoteBtn');
+btn.addEventListener('click', btnClickHandler);
